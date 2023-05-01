3eac938ef552263a425840587d95f4bcd392d043
Justin Yoo <justin.yoojh@gmail.com>
2023-05-01 19:17:03 +0900

    Update endpoint for AOAI chat completion

--- src/WebApi/Controllers/ChatController.cs
+++ src/WebApi/Controllers/ChatController.cs
@@ -2,6 +2,8 @@
 
 using Microsoft.AspNetCore.Mvc;
 
+using WebApi.Configurations;
+using WebApi.Models;
 using WebApi.References.AzureOpenAI;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,48 +14,53 @@ namespace WebApi.Controllers
     [ApiController]
     public class ChatController : ControllerBase
     {
-        // GET: api/<ChatController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        private readonly AzureOpenAISettings _settings;
+        private readonly HttpClient _http;
+        private readonly ILogger<ChatController> _logger;
 
-        // GET api/<ChatController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        public ChatController(AzureOpenAISettings settings, IHttpClientFactory httpClientFactory, ILogger<ChatController> logger)
         {
-            return "value";
+            this._settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            this._http = (httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory))).CreateClient();
+            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         // POST api/<ChatController>
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] string value)
+        [HttpPost()]
+        public async Task<IActionResult> Post([FromBody] ChatCompletionRequest value)
         {
             var accessToken = this.Request.Headers["x-aoai-access-token"];
-            var deploymetId = "model-gpt432k";
-            var apiVersion = "2023-03-15-preview";
-            Body3 body = new Body3();
-            HttpClient http = new HttpClient();
-            http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            ChatCompletionsClient client = new ChatCompletionsClient(http);
-            Response3 response = await client.CreateAsync(deployment_id: deploymetId, api_version: apiVersion, body: body);
+            var apiKey = "f692512cefd640c683109550e3d77316";
+            var deploymentId = this._settings?.DeploymentId;
+            var apiVersion = this._settings?.ApiVersion;
 
-            var result = response.Choices.First().Message.Content;
+            //this._http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            this._http.DefaultRequestHeaders.Add("api-key", apiKey);
 
-            return new OkObjectResult(result);
-        }
+            var body = new Body3()
+            {
+                Messages = new List<Messages>()
+                {
+                    new Messages()
+                    {
+                        Role = MessagesRole.User,
+                        Content = value.Prompt
+                    },
+                },
+                Temperature = 0.7,
+                N = 1,
+                Max_tokens = 800,
+            };
+            var chatCompletion = new ChatCompletionsClient(this._http) { BaseUrl = this._settings?.BaseUrl };
+            var response = await chatCompletion.CreateAsync(deployment_id: deploymentId, api_version: apiVersion, body: body);
 
-        // PUT api/<ChatController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+            var result = response.Choices
+                                 .FirstOrDefault()?
+                                 .Message
+                                 .Content;
+            var res = new ChatCompletionResponse() { Completion = result };
 
-        // DELETE api/<ChatController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return new OkObjectResult(res);
         }
     }
 }
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.AspNetCore.Rewrite;
+
+using WebApi.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
+var settings = new AzureOpenAISettings();
+builder.Configuration.GetSection(AzureOpenAISettings.Name).Bind(settings);
+builder.Services.AddSingleton(settings);
 
 var app = builder.Build();
 
@@ -14,6 +23,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    // add a new rewrite option to redirect the root to /swagger
+    app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
 }
 
 app.UseHttpsRedirection();
